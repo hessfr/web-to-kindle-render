1ee10a08d9b8d24773656e30dc512170aa37b1f2
SunnyTardigrade <chadfromvermont+github@gmail.com>
2023-08-18 16:18:27 -0400

    Updated Puppeteer headless mode

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', async (req, res) => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.setViewport({ width: 600, height: 800 });
     await page.goto(process.env.SCREENSHOT_URL || 'https://www.wunderground.com/weather/us/vt/dorset');
